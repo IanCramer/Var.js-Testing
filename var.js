d8f64c3497170d775d7c6d9c93cb62ac469e4b79
Kenneth Cramer <i.cramer@live.com>
2019-02-07 13:28:57 -0800

    Test script working. Ready to start writing tests

--- var.js
+++ var.js
@@ -1,5 +1,7 @@
 // Returns true if given null or undefined; otherwise returns false.
-export const isNotDefined = x => (x === null) || (x === undefined);
+/*export*/ const isNotDefined = x => (x === null) || (x === undefined);
+module.exports = isNotDefined;
 
 // Returns true if given null, undefined or empty string.
-export const isEmpty = x => isNotDefined(x) || (x === '');
+/*export*/ const isEmpty = x => isNotDefined(x) || (x === '');
+module.exports = isEmpty;
